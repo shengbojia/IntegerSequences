b7770d0ab3de4bd8186c40233568e8ab86783528
Shengbo Jia <shengbojia@gmail.com>
2019-03-19 22:13:31 -0400

    Added Room entity

--- app/src/main/java/com/shengbojia/integersequences/model/Sequence.kt
+++ app/src/main/java/com/shengbojia/integersequences/model/Sequence.kt
@@ -1,2 +1,22 @@
 package com.shengbojia.integersequences.model
 
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(
+    tableName = "sequences",
+    indices = [Index("numberID")]
+)
+data class Sequence(
+    @PrimaryKey
+    @SerializedName("number")
+    val numberId: Int,
+    @SerializedName("data")
+    val sequenceSnippet: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("formula")
+    val formulas: List<String>
+)

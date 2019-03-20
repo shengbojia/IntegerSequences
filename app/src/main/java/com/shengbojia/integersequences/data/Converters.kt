64864f3824873e91aecee5ceefa2f38d3cec2891
Shengbo Jia <shengbojia@gmail.com>
2019-03-19 22:13:48 -0400

    added type converters

--- app/src/main/java/com/shengbojia/integersequences/data/Converters.kt
+++ app/src/main/java/com/shengbojia/integersequences/data/Converters.kt
@@ -1,2 +1,21 @@
 package com.shengbojia.integersequences.data
 
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class Converters {
+
+    @TypeConverter
+    fun fromListToJson(list: List<String>): String {
+        return Gson().toJson(list)
+    }
+
+    @TypeConverter
+    fun fromJsonToList(json: String): List<String> {
+        val listType = genericType<List<String>>()
+        return Gson().fromJson(json, listType)
+    }
+
+    private inline fun <reified T> genericType() = object : TypeToken<T>() {}.type
+}

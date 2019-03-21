7051f53dfcda78671e95f03e0e8fd0e1e358469f
Shengbo Jia <shengbojia@gmail.com>
2019-03-21 01:21:27 -0400

    added Binding adapters

--- app/src/main/java/com/shengbojia/integersequences/adapter/BindingAdapters.kt
+++ app/src/main/java/com/shengbojia/integersequences/adapter/BindingAdapters.kt
@@ -1,2 +1,12 @@
 package com.shengbojia.integersequences.adapter
 
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("app:sequenceId")
+fun bindSequenceId(view: TextView, id: Int?) {
+    if (id != null) {
+        view.text = "A${id.toString()}"
+    }
+}
+git
